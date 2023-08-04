31b6be877e79ebaa4f2d54d41ab6a59121c78afe
sindhusunki789@gmail.com <sindhusunki789@gmail.com>
2023-08-04 17:15:30 +0530

    first commit

---
+++ MainApp/MainApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int number = int.Parse(Console.ReadLine());
+            if (MySharedLibrary.MyUtility.IsPrime(number))
+                Console.WriteLine($"{number} is a prime number.");
+            else
+                Console.WriteLine($"{number} is not a prime number.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MySharedLibrary/MySharedLibrary/MyUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySharedLibrary
+{
+    public static class MyUtility
+    {
+        public static bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+            if (number == 2) return true;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
